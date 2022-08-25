8f1e81d0cf134c5eb408ad323adcfb7c58bc5829
Morgvn Mdln <morgvn.mdln@gmail.com>
2022-08-25 19:33:49 +0200

    modification du nom du formulaire dans le fichier 'app.py' : render_template('index_form.py')

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index_form.py')
 
 @app.route('/dashboard/')
 def dashboard():
