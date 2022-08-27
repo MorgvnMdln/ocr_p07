ebdb5087c3f6bc126068a3eaa16fe5e4998b3b5f
Morgvn Mdln <morgvn.mdln@gmail.com>
2022-08-27 12:29:29 +0200

    changement URL index form et app.py

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ pred_model = PredictionModel()
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index_form.py')
+'''def index():
+    return render_template('index_form.py')'''
 
 # @app.route('/dashboard/')
 # def dashboard():
     # return render_template("dashboard.html")
 
-@app.route("/api/predict", methods=["GET", "POST"])
+@app.route("/api/predict", methods=["GET"])
 
 def predict():
     msg_data = {}
@@ -34,4 +34,4 @@ def predict():
                     })
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port = 5000, debug = True)
